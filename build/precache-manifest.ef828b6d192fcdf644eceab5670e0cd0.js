4576ac7dd8a6fcb8af31cb3c203cdaec57ddfb55
Luke Strama <strama@ualberta.ca>
2019-01-04 16:53:32 -0700

    build

---
+++ build/precache-manifest.ef828b6d192fcdf644eceab5670e0cd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e277d7091659376cf03a",
+    "url": "/static/js/main.e277d709.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e277d7091659376cf03a",
+    "url": "/static/css/main.de1550cf.chunk.css"
+  },
+  {
+    "revision": "5f85b1d59aeed72488c01a3bc1e0d093",
+    "url": "/index.html"
+  }
+];
